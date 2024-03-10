91f38c0d634bfa06ed80903886b92ccfed3f79e6
Vansh Kalra <vanshkalra645@gmail.com>
2024-03-10 17:12:30 +0530

    Added JobWithCompanyDTO

--- jobms/src/main/java/com/embarkx/jobms/job/impl/JobServiceImpl.java
+++ jobms/src/main/java/com/embarkx/jobms/job/impl/JobServiceImpl.java
@@ -4,6 +4,7 @@ package com.embarkx.jobms.job.impl;
 import com.embarkx.jobms.job.Job;
 import com.embarkx.jobms.job.JobRepository;
 import com.embarkx.jobms.job.JobService;
+import com.embarkx.jobms.job.dto.JobWithCompanyDTO;
 import com.embarkx.jobms.job.external.Company;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -12,6 +13,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class JobServiceImpl implements JobService {
@@ -54,14 +56,23 @@ public class JobServiceImpl implements JobService {
     }
 
     @Override
-    public List<Job> findAll() {
-        RestTemplate restTemplate = new RestTemplate();
-        Company company = restTemplate.getForObject("http://localhost:8081/companies/1", Company.class);
-        System.out.println("COMPANY: " + company.getName());
-        System.out.println("COMPANY: " + company.getId());
-        return jobRepository.findAll();
+    public List<JobWithCompanyDTO> findAll() {
+        List<Job> jobs = jobRepository.findAll();
+        List<JobWithCompanyDTO> jobWithCompanyDTOs = new ArrayList<>();
+
+        return jobs.stream().map(this::convertToDto).collect(Collectors.toList());
     }
 
+    private JobWithCompanyDTO convertToDto(Job job) {
+            JobWithCompanyDTO jobWithCompanyDTO = new JobWithCompanyDTO();
+            jobWithCompanyDTO.setJob(job);
+            RestTemplate restTemplate = new RestTemplate();
+            Company company = restTemplate.getForObject(
+                    "http://localhost:8081/companies/" + job.getCompanyId(),
+                    Company.class);
+            jobWithCompanyDTO.setCompany(company);
+            return jobWithCompanyDTO;
+        }
     @Override
     public void createJob(Job job) {
         jobRepository.save(job);
